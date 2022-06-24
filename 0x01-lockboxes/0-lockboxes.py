06a37575637fe46ab76ac24474689622151d4b6a
kwamboka1 <makorimildred@gmail.com>
2022-06-24 14:05:32 +0000

    [update]:the code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,7 +9,7 @@ def canUnlockAll(boxes):
     """
     n = len(boxes)
     for i in boxes:
-        if len(i) == 0 and i == boxes[n-1]:
+        if len(i) == 0 and i is not boxes[n-1]:
             return False
     for index, keys in enumerate(boxes):
         return True
