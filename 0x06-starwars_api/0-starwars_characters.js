0220593b4c822af9ebbcdcfd76db102a64efab76
kwamboka1 <makorimildred@gmail.com>
2022-08-01 15:06:59 +0000

    [Star]: Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const baseUrl = 'https://swapi-api.hbtn.io/api/';
+
+request.get(`${baseUrl}films/${movieId}`, { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  const result = body.characters;
+  // console.log(result);
+
+  actorsList(result);
+});
+
+function actorsList(result, i = 0) {
+  if (i === result.length) return;
+
+  request(result[i], { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+
+    console.log(body.name);
+    actorsList(result, i + 1);
+  });
+}
