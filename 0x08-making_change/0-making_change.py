6e79dab6abba4cde91af6b8625a51db7b9f1267e
kwamboka1 <makorimildred@gmail.com>
2022-08-09 20:31:19 +0000

    [Change]: Comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A function to determine the fewest number of coins needed to meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """Given a pile of coins of different values"""
+    temp = 0
+    coins.sort(reverse=True)
+
+    if total < 0:
+        return 0
+
+    for coin in coins:
+        if total % coin <= total:
+            temp += total // coin
+            total = total % coin
+
+    return temp if total == 0 else -1
